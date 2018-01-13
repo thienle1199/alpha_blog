d596cc5a1798d5dab60fc18662d9a4904b8e6d06
thienle1199 <thienle.luong@gmail.com>
2018-01-13 10:50:51 +0700

    add login authentication

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
   resources :users, except: [:new]
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
